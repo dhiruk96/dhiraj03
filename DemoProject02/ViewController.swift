0111e1e8f56816be5cb6bcff1ae34addb2774403
Deepak <deepak.khamkar@synergygfs.com>
2023-11-02 12:06:14 +0530

    Initial Commit

---
+++ DemoProject02/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DemoProject02
+//
+//  Created by Apple on 02/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
